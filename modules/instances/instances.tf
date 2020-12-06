a00a6c425ad27ecdb71357a54a50b563d72c952a
Kevin <kraim@free.fr>
2020-12-06 14:49:14 +0100

    Fix bootstrap script

--- modules/instances/instances.tf
+++ modules/instances/instances.tf
@@ -16,17 +16,30 @@ resource "google_compute_instance" "default" {
         }
     }
 
-    metadata_startup_script = "sudo apt-get update && sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common zsh vim"
+    metadata_startup_script = <<-EOF_SCRIPT
+        #!bin/bash
 
-    provisioner "remote-exec" {
-        script = var.script_path
-        connection {
-            type        = "ssh"
-            user        = "kevin_raimbaud"
-            private_key = var.private_key
-            host     = var.ip_address
-        }
-    }
+        sudo apt-get update
+        sudo apt-get install -y \
+            apt-transport-https \
+            ca-certificates \
+            curl \
+            gnupg-agent \
+            software-properties-common \
+            zsh \
+            vim
+        curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+        sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+        sudo apt-get update && sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
+        curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+        cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
+        deb https://apt.kubernetes.io/ kubernetes-xenial main
+        EOF
+        sudo apt-get update
+        sudo apt-get install -y kubelet kubeadm kubectl
+        sudo apt-mark hold kubelet kubeadm kubectl
+    EOF_SCRIPT
 
     tags = ["node"]
 }

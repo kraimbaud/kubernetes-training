9c20e2b97121dfb9446bea0f4475c5e6f3b55c15
Kevin <kraim@free.fr>
2020-12-07 14:40:25 +0100

    Use ZSH as default shell

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -11,6 +11,8 @@ sudo apt-get install -y \
     zsh \
     vim
 
+sudo sed -i 's/bash/zsh/g' /etc/passwd # Use zsh as default shell
+
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
 sudo apt-get update && sudo apt-get install -y docker-ce docker-ce-cli containerd.io

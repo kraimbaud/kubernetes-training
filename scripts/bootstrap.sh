5446e844df800d2c6e0e22999b49233b17608b12
Kevin <kraim@free.fr>
2020-12-08 01:50:05 +0100

    Add config for docker script 'systemd'

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -16,6 +16,27 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debi
 sudo apt-get update && sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 sudo gpasswd -a $USER docker # Run docker without sudo
 
+# Set up the Docker daemon
+cat <<EOF | sudo tee /etc/docker/daemon.json
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2"
+}
+EOF
+
+# Create /etc/systemd/system/docker.service.d
+sudo mkdir -p /etc/systemd/system/docker.service.d
+
+# Restart Docker
+sudo systemctl daemon-reload
+sudo systemctl restart docker
+
+sudo systemctl enable docker
+
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
 deb https://apt.kubernetes.io/ kubernetes-xenial main

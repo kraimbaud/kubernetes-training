efb9aa5e5fbe3d79b1a2fb12e87a75c2d7948833
Kevin <kraim@free.fr>
2020-12-09 14:59:35 +0100

    Add firewall

--- network.tf
+++ network.tf
@@ -7,3 +7,18 @@ resource "google_compute_address" "static" {
   name = "vm-public-address"
 }
 
+resource "google_compute_firewall" "default" {
+  name    = "k8s-firewall"
+  network = google_compute_network.vpc_network.name
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80", "6443"]
+  }
+
+  source_tags = ["node"]
+}

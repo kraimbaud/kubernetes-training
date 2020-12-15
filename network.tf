57650e501b9f5ded1312c85348236bf910f7b956
Kevin <kraim@free.fr>
2020-12-15 18:53:02 +0100

    Separate firewall rules

--- network.tf
+++ network.tf
@@ -2,8 +2,13 @@ resource "google_compute_network" "vpc_network" {
   name = "k8s-node"
 }
 
-resource "google_compute_firewall" "node-access-rule" {
-  name    = "k8s-firewall"
+# We create a public IP address for our google compute instance to utilize
+resource "google_compute_address" "static" {
+  name = "vm-public-address"
+}
+
+resource "google_compute_firewall" "node-join-rule" {
+  name    = "node-join-rule"
   network = google_compute_network.vpc_network.name
 
   allow {
@@ -12,15 +17,26 @@ resource "google_compute_firewall" "node-access-rule" {
 
   allow {
     protocol = "tcp"
-    ports    = ["80", "6443", "30000-32767"]
+    ports    = ["6443"]
   }
 
   source_tags = ["node"]
+}
+
+resource "google_compute_firewall" "node-ingress-rule" {
+  name    = "node-ingress-rule"
+  network = google_compute_network.vpc_network.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80", "30000-32767"]
+  }
+
   source_ranges = ["0.0.0.0/0"]
 }
 
 resource "google_compute_firewall" "ssh-rule" {
-  name = "ssh"
+  name = "ssh-rule"
   network = google_compute_network.vpc_network.name
 
   allow {
@@ -31,7 +47,3 @@ resource "google_compute_firewall" "ssh-rule" {
   source_ranges = ["0.0.0.0/0"]
 }
 
-# We create a public IP address for our google compute instance to utilize
-resource "google_compute_address" "static" {
-  name = "vm-public-address"
-}
